b18b32d1fc3c07b3676d9936c929c7fc6ce0e958
andre-pedro <andrewgoesgaming@gmail.com>
2022-09-30 14:12:31 +0100

    Start solving Problema01 of VideojogosLusofona/lp2_exercicios

---
+++ Exercicios/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     
+             int numAmount;
+
+            Console.WriteLine("How many numbers do you wish to calculate?");
+            numAmount = Convert.ToInt32(Console.ReadLine());
+            int[] inputNumbers = new int[numAmount];
+
+            //User input in each Array slot
+            for(int i = 0; i < inputNumbers.Length; i++) 
+            {
+                Console.WriteLine("Write on the "+ i + " position");
+                inputNumbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            
+            //Write all Array numbers
+            Console.WriteLine("These are the numbers you've input:");
+            for(int i = 0; i < inputNumbers.Length; i++) 
+            {
+                Console.Write(inputNumbers[i] + " ");
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
